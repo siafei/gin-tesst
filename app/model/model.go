4819a1b3ff41e30c31cc49ebae8e3b3d1d23b6b5
siafei <376046266@qq.com>
2021-06-09 22:57:45 +0800

    restart

--- app/model/model.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Model struct {
-	ID  uint32 `gorm:"primary_ke" json:"id"`
-	CreatedOn uint32 `gorm:"default:NULl" json:"created_on"`
-	DeletedOn uint32 `gorm:"default:NUll" json:"deleted_on"`
-}
--- main.go
+++ main.go
@@ -1,40 +1,14 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/siafei/gin-test/global"
-	"github.com/siafei/gin-test/routers"
-	"log"
-	"net/http"
+	"fmt"
 )
 
-func init()  {
-	err := SetupSetting()  //引入配置文件
-	if err != nil {
-		log.Fatalf("init setupSetting err: %v",err)
-	}
-	err = SetupDBEngine()
-	if err != nil {
-		log.Fatalf("init DBEngine err: %v",err)
-	}
-	err = SetupLogger()
-	if err != nil {
-		log.Fatalf("init Logger err: %v",err)
-	}
-}
+
 
 
 func main()  {
-	gin.SetMode(global.ServerSetting.RunModel)   //设置运行模式
-	router := routers.NewRouter()
-	s := &http.Server{
-		Addr: ":"+global.ServerSetting.HttpPort,
-		Handler: router,
-		ReadHeaderTimeout: global.ServerSetting.ReadTimeOut,
-		WriteTimeout: global.ServerSetting.WriteTimeOut,
-		MaxHeaderBytes: 1<<20,
-	}
-	s.ListenAndServe()
+fmt.Println("hello world")
 }
 
 
