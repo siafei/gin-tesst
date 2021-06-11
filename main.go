f1f925b4e5c0c109393cbafb17eb00daf740b5a3
孙赞 <sunzan@shihuituan.com>
2021-06-11 15:41:35 +0800

    11111

--- main.go
+++ main.go
@@ -1,26 +1,19 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"net/http"
-	"siafei/gin-test/router"
-	"time"
-)
-
-
-
+import "siafei/gin-test/internal/rabbitmq"
 
 func main()  {
-	gin.SetMode("debug") //debug	release test
-	r := router.New()
-	s := &http.Server{
-		Addr: ":8080",
-		Handler: r,
-		ReadHeaderTimeout: 10*time.Second,
-		WriteTimeout: 10*time.Second,
-		MaxHeaderBytes: 1<<20,
-	}
-	s.ListenAndServe()
+	//gin.SetMode("debug") //debug	release test
+	//r := router.New()
+	//s := &http.Server{
+	//	Addr: ":8080",
+	//	Handler: r,
+	//	ReadHeaderTimeout: 10*time.Second,
+	//	WriteTimeout: 10*time.Second,
+	//	MaxHeaderBytes: 1<<20,
+	//}
+	//s.ListenAndServe()
+	rabbitmq.Test()
 }
 
 
